6cb5ce06498e68a4f6fb7abea9588a278822761e
Ocre42 <john.askew@devinit.org>
2019-06-13 15:47:35 +0100

    making generation script clearer

--- generate-publishing-stats.sh
+++ generate-publishing-stats.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo "LOG: `date '+%Y-%m-%d %H:%M:%S'` - Starting publisher stats regeneration"
-
+if [ ! -d ../IATI-Registry-Refresher/data ]; then
+	echo "LOG: `date '+%Y-%m-%d %H:%M:%S'` - Cloning IATI-Data-Snapshot"
+    git clone git://dashboard.iatistandard.org/IATI-Data-Snapshot.git ../IATI-Registry-Refresher/data
+fi
 echo "LOG: `date '+%Y-%m-%d %H:%M:%S'` - Running IATI-Registry-Refresher"
 cd ../IATI-Registry-Refresher
-if [ ! -d data ]; then
-    git clone git://dashboard.iatistandard.org/IATI-Data-Snapshot.git IATI-Registry-Refresher/data
-fi
 ./git.sh > ../logs/$(date +\%Y\%m\%d)-rr.log 2>&1
 for i in data/*/; do
     zip -FS -r zips/"`basename $i`.zip" "$i"
