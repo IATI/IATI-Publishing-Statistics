0d3713a21ae3dd6cb1e73bc585073475b79d909e
Alex Miller <alex.k.miller@gmail.com>
2020-03-17 13:50:52 -0400

    Always return float

--- IATI-Dashboard/python2round.py
+++ IATI-Dashboard/python2round.py
@@ -5,7 +5,4 @@ context.rounding = ROUND_HALF_UP
 
 
 def round2(value, digits=0):
-    value_dec = Decimal(value)
-    if digits == 0:
-        return int(value_dec.__round__(0))
-    return value_dec.__round__(digits)
+    return float(Decimal(value).__round__(digits))
